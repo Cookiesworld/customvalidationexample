e11852b9467fd037938720d5bd1e99b9d5674843
John Cooke <jdcooke@hotmail.com>
2022-09-03 14:49:32 +0100

    Added code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import { Form, FormGroup } from 'react-bootstrap';
+import { useState } from 'react';
 
 function App() {
+  const [validated, setValidated] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+  }
+
+  const handleChange = (e) => {
+    setValidated(e.currentTarget.value);
+    if (e.currentTarget.value.includes('test')) {
+      e.currentTarget.setCustomValidity('')
+    } else {
+      e.currentTarget.setCustomValidity('Field must contain \'test\'');
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Simple HTML validation example</h1>
+      <div className='md4'>
+        <Form onSubmit={handleSubmit}>
+          <FormGroup >
+            <Form.Label>Test custom input must contain test</Form.Label>
+            <Form.Control required name="test" onChange={handleChange} value={validated} />
+          </FormGroup>
+          <button type="submit">Submit</button>
+        </Form>
+      </div>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Simple HTML validation example', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Simple HTML validation example/i);
   expect(linkElement).toBeInTheDocument();
 });
